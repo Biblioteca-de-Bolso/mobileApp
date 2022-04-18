a2629c15c82913f9eff3c4b0ea6823063be5ed1b
SamuelLimaCap <samueldelimapessoal@gmail.com>
2022-04-18 15:53:45 -0300

    Add button to logout

--- app/src/main/java/com/bibliotecadebolso/app/ui/home/HomeActivity.kt
+++ app/src/main/java/com/bibliotecadebolso/app/ui/home/HomeActivity.kt
@@ -1,6 +1,10 @@
 package com.bibliotecadebolso.app.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,6 +13,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.bibliotecadebolso.app.R
 import com.bibliotecadebolso.app.databinding.ActivityHomeBinding
+import com.bibliotecadebolso.app.ui.appAccess.AppAccessActivity
+import com.bibliotecadebolso.app.util.Constants
 
 class HomeActivity : AppCompatActivity() {
 
@@ -25,10 +31,48 @@ class HomeActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_home)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_home,
-            R.id.navigation_dashboard,
-            R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.topbar_main_menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.mi_logout -> {
+                removeTokensFromSharedPreferences()
+                returnToLoginAccessActivity()
+            }
+            else -> { }
+        }
+        return true
+    }
+
+    private fun removeTokensFromSharedPreferences() {
+        val prefs = getSharedPreferences(Constants.Prefs.USER_TOKENS, MODE_PRIVATE)
+
+        with(prefs.edit()) {
+            remove(Constants.Prefs.Tokens.ACCESS_TOKEN)
+            remove(Constants.Prefs.Tokens.REFRESH_TOKEN)
+            apply()
+        }
+    }
+
+    private fun returnToLoginAccessActivity() {
+        val homeIntent = Intent(this, AppAccessActivity::class.java)
+
+        startActivity(homeIntent)
+        finish()
     }
 }
