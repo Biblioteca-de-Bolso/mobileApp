7b4d52785565f28e48448af5ed7e762ef1184f48
SamuelLimaCap <samueldelimapessoal@gmail.com>
2022-09-11 23:04:13 -0300

    Add activity book list title and add padding to start

--- app/src/main/java/com/bibliotecadebolso/app/ui/book/gridList/BookListActivity.kt
+++ app/src/main/java/com/bibliotecadebolso/app/ui/book/gridList/BookListActivity.kt
@@ -7,16 +7,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bibliotecadebolso.app.R
 import com.bibliotecadebolso.app.data.model.ReadStatusEnum
 import com.bibliotecadebolso.app.databinding.ActivityBookListBinding
 import com.bibliotecadebolso.app.ui.adapter.BookListAdapter
 import com.bibliotecadebolso.app.ui.bookInfo.BookInfoActivity
 import com.bibliotecadebolso.app.ui.home.ui.bookList.BookListFragment
-import com.bibliotecadebolso.app.util.Constants
-import com.bibliotecadebolso.app.util.Result
-import com.bibliotecadebolso.app.util.RvOnClickListener
-import com.bibliotecadebolso.app.util.SharedPreferencesUtils
+import com.bibliotecadebolso.app.util.*
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
 
@@ -24,13 +22,18 @@ class BookListActivity : AppCompatActivity(), RvOnClickListener {
     private lateinit var binding: ActivityBookListBinding
     private lateinit var bookListAdapter: BookListAdapter
     private lateinit var viewModel: BookListViewModel
+    var isLoadingNewItems = false
+    var scrollWasOnBottom = false
+    private var enumChoosed: ReadStatusEnum? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBookListBinding.inflate(layoutInflater)
+        ContextUtils.setActionBarColor(supportActionBar, this)
+        val actionBarTitle = getString(R.string.label_book)
+        supportActionBar?.title = actionBarTitle
 
         val enumString = intent.extras?.getString("readStatusEnum")
-        var enumChoosed: ReadStatusEnum? = null
 
         if (enumString != null){
             try {
@@ -43,13 +46,19 @@ class BookListActivity : AppCompatActivity(), RvOnClickListener {
 
         viewModel = ViewModelProvider(this)[BookListViewModel::class.java]
 
-        val prefs = getSharedPreferences(Constants.Prefs.USER_TOKENS, MODE_PRIVATE)
-        val accessToken = SharedPreferencesUtils.getAccessToken(prefs)
-        viewModel.getBookList(accessToken, readStatusEnum = enumChoosed)
-
         setupRecyclerView()
         setupBookListObserver()
+
+        if (viewModel.bookListLiveData.value == null) getList()
         setContentView(binding.root)
+    }
+
+    private fun getList() {
+        val prefs = getSharedPreferences(Constants.Prefs.USER_TOKENS, MODE_PRIVATE)
+        val accessToken = SharedPreferencesUtils.getAccessToken(prefs)
+
+        viewModel.getBookList(accessToken, readStatusEnum = enumChoosed)
+
     }
 
 
@@ -95,6 +104,5 @@ class BookListActivity : AppCompatActivity(), RvOnClickListener {
         //val modalBottomSheet = BookItemBottomSheet(position)
         //modalBottomSheet.show(this.parentFragmentManager, BookItemBottomSheet.TAG)
     }
-
 
 }
